40918588df7cb21668d28f6f36771603238f17b8
Mateus Antonio Bueno <128940046+Mateus-Bueno@users.noreply.github.com>
2023-12-26 12:29:48 -0300

    Entrega do desafio de POO da trilha .NET DIO

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("(11)99999-9999", "Modelo a", "111111111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("(11)93333-3333", "Modelo b", "222222222222222", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
